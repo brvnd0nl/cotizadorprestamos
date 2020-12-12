598659fcac939dccc6ca9b5dfb4d0a58ea481bde
BrandonMarchena <brandon.marchena@aplinet.co>
2020-12-12 18:42:09 -0500

    Clases

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StatusBar,
   SafeAreaView,
   StyleSheet,
   View,
   Text,
+  Button,
 } from 'react-native';
 import colors from './src/utils/colors';
+import Form from './src/components/Form';
 
 const App = () => {
+  const [capital, setCapital] = useState(null);
+  const [intereses, setIntereses] = useState(null);
+  const [meses, setMeses] = useState(null);
+
+  const onSubmit = () => {
+    console.log('capital ->', capital)
+    console.log('intereses ->', intereses)
+    console.log('meses ->', meses)
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
       <SafeAreaView style={styles.safeArea}>
-        <Text>Formulario HEAD</Text>
+        <View style={styles.background} />
+        <Text style={styles.tittleApp}>Cotizador de Prestamos</Text>
+        <Form 
+          setCapital={setCapital}
+          setIntereses={setIntereses}
+          setMeses={setMeses}
+        />
       </SafeAreaView>
       
       <View>
@@ -22,6 +40,7 @@ const App = () => {
 
       <View>
         <Text>Footer</Text>
+        <Button title="Consultar" onPress={onSubmit} />
       </View>
     </>
   );
@@ -29,11 +48,25 @@ const App = () => {
 
 const styles = StyleSheet.create({
   safeArea:{
+    height: 290,    
+    alignItems:"center"
+  },
+  background: {
     backgroundColor:colors.PRIMARY_COLOR,
     height: 200,
+    width: "100%",
     borderBottomLeftRadius: 30,
     borderBottomRightRadius: 30,
-    alignItems:"center"
+    position: "absolute",
+    zIndex: -1,
+
+  },
+  tittleApp: {
+    fontSize:25,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 15,
+
   }
 });
 
